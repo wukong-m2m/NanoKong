4bbc71a55113622b6e412cae20b71a0a6cdf150c
Niels Reijers <nielsreijers@gmail.com>
2011-09-23 20:15:16 +0800

    Minimum case for weird faint led problem.

---
+++ java/examples/ReferenceToNativeObjectTest.java
@@ -0,0 +1,20 @@
+/*
+  LedBlink.java
+ */
+
+import nanovm.avr.*;
+
+class ReferenceToNativeObjectTest {
+  public static void main(String[] args) {
+    System.out.println("System clock = " + AVR.getClock() + "000 Hz");
+    System.out.println("Blinking LED");
+
+	Port port = AVR.portA;
+
+	for(int i=0; i<8; i++)
+      AVR.portA.setOutput(i);
+    
+    for(int i=0; i<8; i++)
+	  AVR.portA.setBit(i);
+  }
+}
