ceb524bea717ec07a477aa7ee11135543c09d08b
Niels Reijers <nielsreijers@gmail.com>
2011-10-24 22:46:19 +0800

    Work in progress on Z-Wave
    Copied and modified SerialAPI_request and ZW_sendData from testrtt.c.

--- vm/src/nvmcomm_zwave.c
+++ vm/src/nvmcomm_zwave.c
@@ -3,19 +3,30 @@
 #include "config.h"
 #include "uart.h"
 #include "debug.h"
+#include "delay.h"
 
 #ifdef NVM_USE_COMMZWAVE
 
-#define ZWAVE_UART             1
+#define ZWAVE_UART              1
 
-#define ZWAVE_STATUS_SOF       1
-#define ZWAVE_STATUS_LEN       2
-#define ZWAVE_STATUS_TYPE      3
-#define ZWAVE_STATUS_CMD       4
-#define ZWAVE_STATUS_ID        5  
-#define ZWAVE_STATUS_DATA      6
-#define ZWAVE_STATUS_CRC       7
-#define ZWAVE_STATUS_DONE      8
+#define ZWAVE_STATUS_WAIT_ACK        0
+#define ZWAVE_STATUS_WAIT_SOF        1
+#define ZWAVE_STATUS_WAIT_LEN        2
+#define ZWAVE_STATUS_WAIT_TYPE       3
+#define ZWAVE_STATUS_WAIT_CMD        4
+#define ZWAVE_STATUS_WAIT_ID         5
+#define ZWAVE_STATUS_WAIT_DATA       6
+#define ZWAVE_STATUS_WAIT_CRC        7
+#define ZWAVE_STATUS_WAIT_DONE       8
+
+#define ZWAVE_TYPE_REQ          0x00
+#define ZWAVE_TYPE_CMD          0x01
+
+#define ZWAVE_CMD_APPLICATIONCOMMANDHANDLER 0x04
+
+#define ZWAVE_REQ_SENDDATA     0x13
+
+#define ZWAVE_ACK              0x06
 
 // u16_t g_seq = 0;
 u08_t state;        // Current state
@@ -27,15 +38,104 @@ u08_t payload_length;  // Length of the payload while reading a packet
 // TODO: used?
 u08_t last_node = 0;
 u08_t seq;          // Sequence number which is used to match the callback function
+u08_t ack_got = 0;
 // u32_t expire;  // The expire time of the last command
 void (*f)(address_t src, u08_t *payload, u08_t length); // The callback function registered by callback
 void (*f_nodeinfo)(u08_t *payload, u08_t length);
 
-// Private
-void display_nif(u08_t *payload, u08_t len) {
-    DEBUGF_COMM("Status=%d Node=%d Device=%d:%d:%d\n", payload[0],payload[1],payload[3],payload[4],payload[5]);
-    last_node = payload[1];
+
+//===================================================================================================================
+// Copied & modified from testrtt.c
+//===================================================================================================================
+int SerialAPI_request(unsigned char *buf, int len)
+{
+	unsigned char c = 1;
+	int i;
+	unsigned char crc;
+	int retry = 5;
+
+	while (1) {
+		// read out pending request from Z-Wave
+    nvmcomm_zwave_poll();
+		if (state != ZWAVE_STATUS_WAIT_SOF) {	// wait for WAIT_SOF statie (idle state)
+			DEBUGF_COMM("SerialAPI is not in ready state!!!!!!!!!! zstate="DBG8"\n", state);
+			DEBUGF_COMM("Try to send SerialAPI command in a wrong state......\n");
+			delay(MILLISEC(100));
+			//continue;
+		}
+	
+		// send SerialAPI request
+		c=1;
+		uart_write_byte(ZWAVE_UART, c); // SOF (start of frame)
+		c = len+1;
+		uart_write_byte(ZWAVE_UART, c); // len (length of frame)
+		crc = 0xff;
+		crc = crc ^ (len+1);
+		for(i=0;i<len;i++) {
+			crc = crc ^ buf[i];
+			uart_write_byte(ZWAVE_UART, buf[i]); // REQ, cmd, data
+    }
+    uart_write_byte(ZWAVE_UART, crc); // LRC checksum
+#ifdef DEBUG
+		DEBUGF_COMM("Send len="DBG8" ", len+1);
+		for(i=0;i<len;i++) 
+			DEBUGF_COMM(""DBG8" ", buf[i]);
+			DEBUGF_COMM("CRC="DBG8"\n", crc);
+#endif
+		state = ZWAVE_STATUS_WAIT_ACK;
+		ack_got = 0;
+
+		// get SerialAPI ack
+		while(1) {
+      i = 0;
+      while(!uart_available(ZWAVE_UART) && i++<1000)
+        delay(MILLISEC(1));
+      if (uart_available(ZWAVE_UART)) {
+        nvmcomm_zwave_poll();			
+  			if (ack_got == 1) {
+  				return 0;
+  			} else {
+  				DEBUGF_COMM("Ack error!!! zstate="DBG8" ack_got="DBG8"\n", state, ack_got);
+  			}
+		  } else {
+				DEBUGF_COMM("No ack!!! zstate="DBG8" ack_got="DBG8"\n", state, ack_got);
+		  }
+		}
+		if (!retry--) {
+			DEBUGF_COMM("SerialAPI request:\n");
+			for (i=0; i<len; i++) {
+				DEBUGF_COMM(""DBG8" ", buf[i]);
+			}
+			DEBUGF_COMM("\n");
+			DEBUGF_COMM("error!!!\n", __FUNCTION__);
+			return -1;
+		}
+		DEBUGF_COMM("SerialAPI_request retry......\n");
+	}
+	return -1; // Never happens
 }
+
+int ZW_sendData(uint8_t id, u08_t *in, u08_t len, u08_t txoptions)
+{
+	unsigned char buf[64]; // TODO: buffersize
+  int i;
+  
+	buf[0] = ZWAVE_TYPE_REQ;
+	buf[1] = ZWAVE_REQ_SENDDATA;
+	buf[2] = id;
+	buf[3] = len;
+  for(i=0; i<len; i++)
+    buf[i+4] = in[i];
+	buf[4+len] = txoptions;
+  buf[5+len] = seq++;
+	return SerialAPI_request(buf, len + 6);
+}
+//===================================================================================================================
+// End: copied & modified from testrtt.c
+//===================================================================================================================
+
+
+
 
 // Blocking receive.
 // Returns the Z-Wave cmd of the received message.
@@ -46,36 +146,43 @@ void nvmcomm_zwave_receive(void) {
 // TODO    expire = now + 1000;
     u08_t c = uart_read_byte(ZWAVE_UART);
     DEBUGF_COMM("c="DBG8" state="DBG8"\n\r", c, state);
-    if (state == ZWAVE_STATUS_SOF) {
-      if (c == 0x01) {
-        state = ZWAVE_STATUS_LEN;
-        len = 0;
-      } else if (c == 0x15) {
-        // TODO: send: packet is incorrect
+    if (state == ZWAVE_STATUS_WAIT_ACK) {
+      if (c == ZWAVE_ACK) {
+  			state = ZWAVE_STATUS_WAIT_SOF;
+  			ack_got=1;
+  		} else if (c == 0x15) {
+          // TODO: send: packet is incorrect
       } else if (c == 0x18) {
-        // TODO: send: chip busy
-      } else if (c == 0x06) {
-        // TODO: send: correct
+          // TODO: send: chip busy
       }
-    } else if (state == ZWAVE_STATUS_LEN) {
-      len = c-3;
-      state = ZWAVE_STATUS_TYPE;
-    } else if (state == ZWAVE_STATUS_TYPE) {
+    } else if (state == ZWAVE_STATUS_WAIT_SOF) {
+      if (c == 0x01) {
+        state = ZWAVE_STATUS_WAIT_LEN;
+        len = 0;
+      } else if (c == ZWAVE_ACK) {
+  			DEBUGF_COMM("ZWAVE_STATUS_WAIT_SOF: SerialAPI got unknown ACK ????????\n");
+  			ack_got = 1;
+      }
+    } else if (state == ZWAVE_STATUS_WAIT_LEN) {
+      len = c-3; // 3 bytes for TYPE, CMD, and CRC
+      state = ZWAVE_STATUS_WAIT_TYPE;
+    } else if (state == ZWAVE_STATUS_WAIT_TYPE) {
       type = c;
-      state = ZWAVE_STATUS_CMD;
-    } else if (state == ZWAVE_STATUS_CMD) {
+      state = ZWAVE_STATUS_WAIT_CMD;
+    } else if (state == ZWAVE_STATUS_WAIT_CMD) {
       cmd = c;
-      state = ZWAVE_STATUS_DATA;
+      state = ZWAVE_STATUS_WAIT_DATA;
       payload_length = 0;
-    } else if (state == ZWAVE_STATUS_DATA) {
+    } else if (state == ZWAVE_STATUS_WAIT_DATA) {
       payload[payload_length++] = c;
       len--;
       if (len == 0) {
-        state = ZWAVE_STATUS_CRC;
+        state = ZWAVE_STATUS_WAIT_CRC;
       }
-    } else if (state == ZWAVE_STATUS_CRC) {
+    } else if (state == ZWAVE_STATUS_WAIT_CRC) {
       uart_write_byte(ZWAVE_UART, 6);
-      state = ZWAVE_STATUS_SOF;
+      state = ZWAVE_STATUS_WAIT_SOF;
+      if (type == ZWAVE_TYPE_REQ && cmd == ZWAVE_CMD_APPLICATIONCOMMANDHANDLER)
       if (f!=NULL)
         f(payload[1], payload+4, payload_length-4); // Trim off first 4 bytes to get to the data. Byte 1 is the sending node.
       if (cmd == 0x49 && f_nodeinfo)
@@ -94,9 +201,9 @@ void nvmcomm_zwave_init() {
   // randomSeed(analogRead(0));
   // seq = random(255);
   seq = 42; // temporarily init to fixed value
-  state = ZWAVE_STATUS_SOF;
+  state = ZWAVE_STATUS_WAIT_SOF;
   f=NULL;
-  f_nodeinfo=display_nif;
+  f_nodeinfo=NULL;
   uart_init(ZWAVE_UART, ZWAVE_UART_BAUDRATE);
 // TODO
   // expire = 0;
@@ -113,7 +220,7 @@ void nvmcomm_zwave_poll(void) {
   // if (expire && (now > expire)) {
   //   expire = 0;
   //   type = 2;
-  //   state = ZWAVE_STATUS_SOF;
+  //   state = ZWAVE_STATUS_WAIT_SOF;
   //   if (f!=NULL) f(payload,i);
   //   Serial.write("timeout...\n");
   //   return true;
@@ -124,44 +231,23 @@ void nvmcomm_zwave_poll(void) {
 
 // Send ZWave command to another node. This command can be used as wireless repeater between 
 // two nodes. It has no assumption of the payload sent between them.
-void nvmcomm_zwave_send(address_t dest, u08_t *b, u08_t l, u08_t option) {
-  int k;
-  u08_t crc;
-// TODO: buffer size was 24, but aren't Z-Wave packets larger?
-  u08_t buf[64];
-
+void nvmcomm_zwave_send(address_t dest, u08_t *data, u08_t len, u08_t txoptions) {
 #ifdef DEBUG
-  DEBUGF_COMM("Sending message to "DBG8", length "DBG8": ", dest, l);
-  for (size8_t i=0; i<l; ++i) {
-    DEBUGF_COMM(" "DBG8"", b[i]);
+  DEBUGF_COMM("Sending message to "DBG8", length "DBG8": ", dest, len);
+  for (size8_t i=0; i<len; ++i) {
+    DEBUGF_COMM(" "DBG8"", data[i]);
   }
   DEBUGF_COMM("\n");
 #endif
 
-  buf[0] = 1;
-  buf[1] = l+7;
-  buf[2] = 0; // ZW_REQ
-  buf[3] = 0x13; // ZW_SendData
-  buf[4] = dest;
-  buf[5] = l;
-  for(k=0;k<l;k++)
-    buf[k+6] = b[k];
-  buf[l+6] = option;
-  buf[l+7] = seq;
-
-  crc = 0xff;
-  uart_write_byte(ZWAVE_UART, buf[0]);
-  for(k=0;k<l+7;k++) {
-    uart_write_byte(ZWAVE_UART, buf[k+1]);
-    crc = crc ^ buf[k+1];
-  }
-  seq++;
-  uart_write_byte(ZWAVE_UART, crc);
+  ZW_sendData(dest, data, len, txoptions);
 // TODO  expire = millis()+1000;
 }
 
 
-//===================================================================================================================
+
+
+
 
 
 
