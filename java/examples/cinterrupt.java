24ed8018ac2eebe19d6a8ba63b4f18f05f728494
brotherwin <brotherwin2002@yahoo.com.tw>
2011-10-26 23:48:31 +0800

    external interrupt PCINT0~23

--- java/examples/cinterrupt.java
+++ java/examples/cinterrupt.java
@@ -10,13 +10,18 @@ class cinterrupt {
 		//connect LED between GND and portA0(digital 22)
 		AVR.portD.setInput(0);	//INT0
 		AVR.portD.setBit(0);	//pull high
+		AVR.portB.setInput(0);	//PCINT0
+		AVR.portB.setBit(0);	//pull high
 		AVR.portA.setOutput(0);
 		AVR.portA.clrBit(0);	//initial A to low
-
+		AVR.portD.setOutput(1);	
 		while(true)
 		{
-
-			System.out.println("touch portD0(digital 21) with GND to create interrupt (INT0)");
+			//connect portD1(digital 20) with PCINT0(digital 53) can see interrupt
+			AVR.portD.clrBit(1);	
+			Timer.wait(1000);
+			System.out.println("touch portD0(digital 21) with GND to create interrupt (INT0)");");
+			AVR.portD.setBit(1);
 			Timer.wait(1000);
 		}
 
