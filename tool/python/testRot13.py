d76df05ccfb8c6900781d8f85c17facc67a07dc6
Niels Reijers <nielsreijers@gmail.com>
2011-11-15 18:16:27 +0800

    Made target node a parameter in python test and reprogramming scripts.

--- tool/python/testRot13.py
+++ tool/python/testRot13.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python
 
+import sys
 import pynvc3
 
-def testRot13():
+def testRot13(destination):
   input = "Hallo";
   print "Input: ", input
-  pynvc3.sendcmd(pynvc3.APPMSG, [ord(c) for c in input])
+  pynvc3.sendcmd(destination, pynvc3.APPMSG, [ord(c) for c in input])
   pynvc3.receive(5000) # Receive ack of APPMSG
   reply = pynvc3.receive(5000) # Receive reply
-  pynvc3.sendcmd(pynvc3.APPMSG, [pynvc3.APPMSG_STATUS_ACK])
+  pynvc3.sendcmd(destination, pynvc3.APPMSG, [pynvc3.APPMSG_STATUS_ACK])
   print "Reply:", [chr(c) for c in reply[1:]] # Strip of the leading 0x7e (APPMSG) command
 
-pynvc3.init()
+pynvc3.init(int(sys.argv[1]))
 testRot13()
