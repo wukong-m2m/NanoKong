ca9ef04accb2cacb6e20ec76dab73ec874112dc4
Niels Reijers <nielsreijers@gmail.com>
2011-11-25 10:56:58 +0800

    Fixed bug in nvmcomm rot13 test.

--- tool/python/testRot13.py
+++ tool/python/testRot13.py
@@ -3,8 +3,7 @@
 import sys
 import pynvc3
 
-def testRot13(destination):
-  input = "Hallo";
+def testRot13(destination, input):
   print "Input: ", input
   pynvc3.sendcmd(destination, pynvc3.APPMSG, [ord(c) for c in input])
   pynvc3.receive(5000) # Receive ack of APPMSG
@@ -12,5 +11,5 @@ def testRot13(destination):
   pynvc3.sendcmd(destination, pynvc3.APPMSG, [pynvc3.APPMSG_STATUS_ACK])
   print "Reply:", [chr(c) for c in reply[1:]] # Strip of the leading 0x7e (APPMSG) command
 
-pynvc3.init(int(sys.argv[1]))
-testRot13()
+pynvc3.init()
+testRot13(int(sys.argv[1]), sys.argv[2] if len(sys.argv)>2 else "Hallo")
