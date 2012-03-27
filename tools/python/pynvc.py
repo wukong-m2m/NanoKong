e5db259e9390d61d1f15d810aa4bef6f31824448
Niels Reijers <nielsreijers@gmail.com>
2012-03-27 11:13:14 +0800

    Copied fixed versions of pynvc.py and reprogram.py from inteldemo back to normal python directory.

--- tools/python/pynvc.py
+++ tools/python/pynvc.py
@@ -27,13 +27,82 @@ RUNLVL_VM                    = 0x02
 RUNLVL_CONF                  = 0x03
 RUNLVL_RESET                 = 0x04
 
-def sendcmd(dest, cmd, payload=[]):
-  pyzwave.send(dest, [0x88, cmd] + payload)
-  if cmd == APPMSG:
-    print pyzwave.receive(5000) # Receive ack of APPMSG, TODO: see if sending succeeded.
+def sendcmd(dest, cmd, payload=[], retries=3):
+  pyzwave.receive(10) # Clear pending messages
+  while retries >= 0:
+    try:
+      pyzwave.send(dest, [0x88, cmd] + payload)
+    except:
+      print "=============IOError============ retries remaining:"
+      print retries
+      if retries <= 0:
+        raise
+      else:
+        retries -= 1
+    else:
+      if cmd == APPMSG:
+        ack = pyzwave.receive(5000) # Receive ack of APPMSG, TODO: see if sending succeeded.
+        print "APPMSG ACK:", ack
+      return
 
-def receive(waitmsec):
+def receive(waitmsec=1000):
   return pyzwave.receive(waitmsec)
 
+def checkedReceive(allowedReplies, waitmsec=1000):
+  reply = pyzwave.receive(waitmsec)
+  if reply == None:
+    print "No reply received. One of", allowedReplies, "expected."
+    return None
+  if not reply[0] in allowedReplies:
+    print "Incorrect reply received. One of", allowedReplies, "expected, but got", reply
+    return None
+  return reply
+
+def sendWithRetryAndCheckedReceive(destination, command, allowedReplies, payload=[], waitmsec=1000, retries=10, quitOnFailure=False):
+  while retries >= 0:
+    try:
+      sendcmd(destination, command, payload)
+      reply = checkedReceive(allowedReplies, waitmsec)
+      if not reply == None:
+        return reply
+    except:
+      pass
+    retries -= 1
+  if quitOnFailure:
+    print "=====FAIL====="
+    print "No reply received for command:"
+    print command
+    print "payload:"
+    print payload
+    print "Aborting"
+    quit()
+  else:
+    return None
+
+def sendWithRetryAndCheckedReceiveAPPMSG(destination, command, allowedReplies, payload=[], waitmsec=1000, retries=10, quitOnFailure=False):
+  while retries >= 0:
+    try:
+      sendcmd(destination, APPMSG, [command] + payload)
+      reply = checkedReceive([APPMSG], waitmsec)
+      if not reply == None and len(reply) > 1 and reply[1] in allowedReplies:
+        return reply[1:]
+      else:
+        print "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+        print reply
+        print reply[1]
+        print allowedReplies 
+    except:
+      pass
+    retries -= 1
+  if quitOnFailure:
+    print "=====FAIL====="
+    print "No reply received for command:"
+    print command
+    print "payload:"
+    print payload
+    print "Aborting"
+    quit()
+  else:
+    return None
 def init():
   pyzwave.init("10.3.36.231")
