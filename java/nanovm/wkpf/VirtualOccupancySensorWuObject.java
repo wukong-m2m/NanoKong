ece70994f3e9213c91489455cbf997c0b17d6ab7
Niels Reijers <nielsreijers@gmail.com>
2012-05-13 16:46:04 +0800

    Removed WKPF_PROP_BASE for common properties. Sampling rate will be a special property type instead.

--- java/nanovm/wkpf/VirtualOccupancySensorWuObject.java
+++ java/nanovm/wkpf/VirtualOccupancySensorWuObject.java
@@ -6,7 +6,7 @@ public class VirtualOccupancySensorWuObject extends VirtualWuObject {
     };
 
     public static final short WUCLASS_OCCUPANCY_SENSOR                           = 0x1005;
-    public static final byte PROPERTY_OCCUPANCY_SENSOR_OCCUPIED                  = (WKPF.PROP_BASE+0);
+    public static final byte PROPERTY_OCCUPANCY_SENSOR_OCCUPIED                  = 0;
 
     public void update() {
       System.out.println("WKPFUPDATE(OccupancySensor): NOP");
