6e29676d1e8a22a4e72479e78c7bdea5534956aa
Niels Reijers <nielsreijers@gmail.com>
2012-05-15 02:47:29 +0800

    Separated generated Java code into files prefixed with GENERATED.

--- java/nanovm/wkpf/WKPF.java
+++ java/nanovm/wkpf/WKPF.java
@@ -67,10 +67,11 @@ public class WKPF {
   public static final byte PROPERTY_THRESHOLD_THRESHOLD                        = 1;
   public static final byte PROPERTY_THRESHOLD_VALUE                            = 2;
   public static final byte PROPERTY_THRESHOLD_OUTPUT                           = 3;
-  public static final short PROPERTY_THRESHOLD_OPERATOR_GT                     = 0;
-  public static final short PROPERTY_THRESHOLD_OPERATOR_LT                     = 1;
-  public static final short PROPERTY_THRESHOLD_OPERATOR_GTE                    = 2;
-  public static final short PROPERTY_THRESHOLD_OPERATOR_LTE                    = 3;
+
+  public static final short ENUM_THRESHOLD_OPERATOR_GT                         = 0;
+  public static final short ENUM_THRESHOLD_OPERATOR_LT                         = 1;
+  public static final short ENUM_THRESHOLD_OPERATOR_GTE                        = 2;
+  public static final short ENUM_THRESHOLD_OPERATOR_LTE                        = 3;
 
   public static final short WUCLASS_TEMPERATURE_SENSOR                         = 2;
   public static final byte PROPERTY_TEMPERATURE_SENSOR_CURRENT_TEMPERATURE     = 0;
@@ -89,5 +90,8 @@ public class WKPF {
   public static final byte PROPERTY_AND_GATE_IN1                               = 0;
   public static final byte PROPERTY_AND_GATE_IN2                               = 1;
   public static final byte PROPERTY_AND_GATE_OUTPUT                            = 2;
+  
+  public static final short WUCLASS_OCCUPANCY_SENSOR                           = 7;
+  public static final byte PROPERTY_OCCUPANCY_SENSOR_OCCUPIED                  = 0;  
 }
 
