79d50b4331344d3e04b1f722efb9fe12fa01bf4d
Niels Reijers <nielsreijers@gmail.com>
2012-05-16 19:03:44 +0800

    small fix

--- vm/src/native_wuclasses/wuclass_numeric_controller_update.c
+++ vm/src/native_wuclasses/wuclass_numeric_controller_update.c
@@ -2,10 +2,10 @@
 #include <wkpf.h>
 #include "native_wuclasses.h"
 
-#ifdef ENABLE_WUCLASS_NUMERIC_CONTROLER
+#ifdef ENABLE_WUCLASS_NUMERIC_CONTROLLER
 
 void wuclass_numeric_controller_update(wkpf_local_wuobject *wuobject) {
   DEBUGF_WKPFUPDATE("WKPFUPDATE(NumericController): NOP\n");
 }
 
-#endif // ENABLE_WUCLASS_NUMERIC_CONTROLER
+#endif // ENABLE_WUCLASS_NUMERIC_CONTROLLER
