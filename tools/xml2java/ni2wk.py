43bbb7a62b4c30e927720827fcd6891a772e4743
bltsai <l@nb.(none)>
2012-05-27 23:46:08 +0800

    Niagara2WuKong code generation done

--- tools/xml2java/ni2wk.py
+++ tools/xml2java/ni2wk.py
@@ -8,24 +8,22 @@
 import os
 import xml.etree.ElementTree as ET
 from optparse import OptionParser
-from operator import itemgetter
-
-out_fd = None
 
 def parser():
     parser = OptionParser("usage: %prog [options]")
     parser.add_option("-i", "--input", action="store", type="string", dest="i", metavar="INPUT", help="Input: Niagara FBP XML file path")
-    parser.add_option("-o", "--output", action="store", type="string", dest="out", metavar="OUTPUT", help="Output: WuKong Flow XML file path", default="output.xml")
-    parser.add_option("-n", "--name", action="append", type="string", dest="nl", metavar="NAMELIST", help='Names of the scenarios "-n A -n B ..."', default=[])
+    parser.add_option("-n", "--name", action="append", type="string", dest="nl", metavar="NAMELIST", help='Name(s) of the scenario(s) "-n A -n B ..."', default=[])
+    parser.add_option("-o", "--output-path", action="store", type="string", dest="out", metavar="OUTPUT_DIR", help="the dest. folder path of output java file")
     
     (options, args) = parser.parse_args()
     if not options.i: parser.error("invalid Nigara XML, please refer to -h for help")
+    out_dir = options.out
+    if not out_dir: out_dir = os.getcwd()
+    assert os.path.exists(out_dir), 'Error! the specified output directory "-o %s" does not exist.' % out_dir
     if len(options.nl) == 0: parser.error("invalid Scenario name(s), please refer to -h for help")
 
     root = ET.parse(options.i).getroot()
-    global out_fd
-    out_fd = open(options.out, 'w')
-
+    
     plist = root.findall(".//p[@t='b:Folder']")
     slist = [p for p in plist if p.get('n') in options.nl]
     olist = []
@@ -36,8 +34,14 @@ def parser():
             c.get('h'):{'type':c.get('t'), 'iname':c.get('n'), 'link':{},
                         'val':{ i.get('n'):i.get('v') for i in list(set(c.findall("p"))-set(c.findall("p[@t]"))) }
             } for c in clist 
-        } # id: name, value_dict, link_dict
-        # rename the type, add links to source component in the view of target component
+        } 
+        # id2comp format
+        # id: { 'type': type (wuClass),
+        #       'iname': instancdId, 
+        #       'val': default_value_dict {prop_name: prop_default}
+        #       'link': links_dict {fromProperty: (toInstanceId, toProperty) 
+        #     }
+        ## rename the type, add links to source component in the view of target component
         for component in clist:
             dest = id2comp[component.get('h')]
             dest['type'] = dest['type'][dest['type'].find(':')+1:]
@@ -54,14 +58,26 @@ def parser():
                 src['link'][sprop] = (dname, dprop)
                 # only destination property cannot be set with default value
                 if dprop in dval: del dval[dprop]
-        olist += [id2comp]
+        olist += [(scenario.get('n'), id2comp)]
+    return out_dir, olist
 
-    return options.out, olist
-
-def codegen(scenarios):
-    print >> out_fd, 'Ha'
+def codegen(out_dir, slist):
+    from xml.dom.minidom import parseString
+    for scenario in slist:
+        root = ET.Element('application', attrib={'name':scenario[0]})
+        out_filename = scenario[0] + ".xml"
+        for c in scenario[1].values():
+            component = ET.SubElement(root, 'component', attrib={'type':c['type'], 'instancdId':c['iname']})
+            for k, v in c['val'].iteritems():
+                ET.SubElement(component, 'property', attrib={'name':k, 'default':v})
+            for k, v in c['link'].iteritems():
+                ET.SubElement(component, 'link', attrib={'fromProperty':k, 'toInstanceId':v[0], 'toProperty':v[1]})
+        fd = open(os.path.join(out_dir, out_filename), 'w')
+        parseString(ET.tostring(root)).writexml(fd, addindent=" "*4, newl='\n')
+        fd.close()
+        del root
+        print "Niagara2WuKongFlow msg: the file %s is generated in the folder %s" % (out_filename, out_dir)
 
 if __name__ == "__main__":
-    out, scenarios = parser()
-    codegen(scenarios)
-    print "Niagara2WuKongFlow msg: the file %s is generated" % (out)
+    out_dir, slist = parser()
+    codegen(out_dir, slist)
