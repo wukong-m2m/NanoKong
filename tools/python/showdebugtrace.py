efff6365559a9703c5d9d8b8435f811b4474ad1b
Niels Reijers <nielsreijers@gmail.com>
2012-05-31 17:07:33 +0800

    work in progress

--- tools/python/showdebugtrace.py
+++ tools/python/showdebugtrace.py
@@ -2,10 +2,19 @@
 
 import pynvc
 
+messages_in_progress = {}
+
+pynvc.init(0)
 while(true):
-  
-  
-  
-  DEBUG_TRACE_PART             = 0xA0
-  DEBUG_TRACE_FINAL            = 0xA2
-  
+  src, message = pynvc.receive(waitmsec)
+  command = message[0]
+  debugtext = message[1:]
+  if command == pynvc.DEBUG_TRACE_PART:
+    if not messages_in_progress.has_key(src):
+      messages_in_progress[src] = ""
+    messages_in_progress[src] += debugtext
+  if command == DEBUG_TRACE_FINAL:
+    if not messages_in_progress.has_key(src):
+      debugtext = messages_in_progress[src] + debugtext
+      messages_in_progress[src] = ""
+    print "Node %d) %s" % (src, debugtext)
