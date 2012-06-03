659d7bb9a4bdc3932f687e9ae930dc3a51e66efb
brotherwin <brotherwin2002@yahoo.com.tw>
2012-06-03 12:54:22 +0800

    java test done

--- java/examples/TestWKPF.java
+++ java/examples/TestWKPF.java
@@ -107,28 +107,153 @@ public class TestWKPF {
 
     assertEqualBoolean(WKPF.getPropertyBoolean(wuclassInstanceThreshold, WKPF.PROPERTY_THRESHOLD_OUTPUT), true, "Getting output of virtual threshold wuclass, should be true.");
 
-    
+    //condition selector test
+    /*VirtualWuObject wuclassInstanceConditionSelectorBoolean = new VirtualConditionSelectorBooleanWuObject();
+    WKPF.registerWuClass(WKPF.WUCLASS_CONDITION_SELECTOR_BOOLEAN, GENERATEDVirtualConditionSelectorBooleanWuObject.properties);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "Registering VirtualConditionSelectorBooleanWuObject.");
+    WKPF.createWuObject((short)WKPF.WUCLASS_CONDITION_SELECTOR_BOOLEAN, (byte)0x23, wuclassInstanceConditionSelectorBoolean);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "Creating wuobject for virtual condition selector boolean wuclass at port 0x23.");
+    WKPF.setPropertyBoolean(wuclassInstanceConditionSelectorBoolean, WKPF.PROPERTY_CONDITION_SELECTOR_BOOLEAN_INPUT, true);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "input = true");
+    WKPF.setPropertyBoolean(wuclassInstanceConditionSelectorBoolean, WKPF.PROPERTY_CONDITION_SELECTOR_BOOLEAN_CONTROL, false);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "control = false");
+    wuclassInstanceConditionSelectorBoolean.update();
+    assertEqualBoolean(WKPF.getPropertyBoolean(wuclassInstanceConditionSelectorBoolean, WKPF.PROPERTY_CONDITION_SELECTOR_BOOLEAN_OUTPUT1), true, "output1 should be true.");
+    assertEqualBoolean(WKPF.getPropertyBoolean(wuclassInstanceConditionSelectorBoolean, WKPF.PROPERTY_CONDITION_SELECTOR_BOOLEAN_OUTPUT2), false, "output2 should be false.");
+    WKPF.setPropertyBoolean(wuclassInstanceConditionSelectorBoolean, WKPF.PROPERTY_CONDITION_SELECTOR_BOOLEAN_INPUT, true);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "input = true");
+    WKPF.setPropertyBoolean(wuclassInstanceConditionSelectorBoolean, WKPF.PROPERTY_CONDITION_SELECTOR_BOOLEAN_CONTROL, true);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "control = true");
+    wuclassInstanceConditionSelectorBoolean.update();
+    assertEqualBoolean(WKPF.getPropertyBoolean(wuclassInstanceConditionSelectorBoolean, WKPF.PROPERTY_CONDITION_SELECTOR_BOOLEAN_OUTPUT1), false, "output1 should be false.");
+    assertEqualBoolean(WKPF.getPropertyBoolean(wuclassInstanceConditionSelectorBoolean, WKPF.PROPERTY_CONDITION_SELECTOR_BOOLEAN_OUTPUT2), true, "output2 should be true.");
 
-    VirtualWuObject wuclassInstanceLogical = new VirtualLogicalWuObject();
+    VirtualWuObject wuclassInstanceConditionSelectorShort = new VirtualConditionSelectorShortWuObject();
+    WKPF.registerWuClass(WKPF.WUCLASS_CONDITION_SELECTOR_SHORT, GENERATEDVirtualConditionSelectorShortWuObject.properties);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "Registering VirtualConditionSelectorShortWuObject.");
+    WKPF.createWuObject((short)WKPF.WUCLASS_CONDITION_SELECTOR_SHORT, (byte)0x24, wuclassInstanceConditionSelectorShort);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "Creating wuobject for virtual condition selector short wuclass at port 0x24.");
+    WKPF.setPropertyShort(wuclassInstanceConditionSelectorShort, WKPF.PROPERTY_CONDITION_SELECTOR_SHORT_INPUT, (short)60);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "input = true");
+    WKPF.setPropertyBoolean(wuclassInstanceConditionSelectorShort, WKPF.PROPERTY_CONDITION_SELECTOR_SHORT_CONTROL, false);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "control = false");
+    wuclassInstanceConditionSelectorShort.update();
+    assertEqual(WKPF.getPropertyShort(wuclassInstanceConditionSelectorShort, WKPF.PROPERTY_CONDITION_SELECTOR_SHORT_OUTPUT1), 60, "output1 should be 60.");
+    assertEqual(WKPF.getPropertyShort(wuclassInstanceConditionSelectorShort, WKPF.PROPERTY_CONDITION_SELECTOR_SHORT_OUTPUT2), 0, "output2 should be 0.");
+    WKPF.setPropertyShort(wuclassInstanceConditionSelectorShort, WKPF.PROPERTY_CONDITION_SELECTOR_SHORT_INPUT, (short)70);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "input = 70");
+    WKPF.setPropertyBoolean(wuclassInstanceConditionSelectorShort, WKPF.PROPERTY_CONDITION_SELECTOR_SHORT_CONTROL, true);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "control = true");
+    wuclassInstanceConditionSelectorShort.update();
+    assertEqual(WKPF.getPropertyShort(wuclassInstanceConditionSelectorShort, WKPF.PROPERTY_CONDITION_SELECTOR_SHORT_OUTPUT1), 0, "output1 should be 0.");
+    assertEqual(WKPF.getPropertyShort(wuclassInstanceConditionSelectorShort, WKPF.PROPERTY_CONDITION_SELECTOR_SHORT_OUTPUT2), 70, "output2 should be 70.");*/
 
-   /* WKPF.registerWuClass(WKPF.WUCLASS_LOGICAL, GENERATEDVirtualLogicalWuObject.properties);
+
+    //MATH TEST
+    /*VirtualWuObject wuclassInstanceMath = new VirtualMathWuObject();
+    WKPF.registerWuClass(WKPF.WUCLASS_MATH, GENERATEDVirtualMathWuObject.properties);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "Registering VirtualMathWuObject.");
+    WKPF.createWuObject((short)WKPF.WUCLASS_MATH, (byte)0x22, wuclassInstanceMath);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "Creating wuobject for virtual math wuclass at port 0x22.");
+
+    WKPF.setPropertyShort(wuclassInstanceMath, WKPF.PROPERTY_MATH_OPERATOR, WKPF.ENUM_MATH_OPERATOR_MAX);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "operator = MAX");
+    WKPF.setPropertyShort(wuclassInstanceMath, WKPF.PROPERTY_MATH_INPUT1, (short)40);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "input1 = 40");
+    WKPF.setPropertyShort(wuclassInstanceMath, WKPF.PROPERTY_MATH_INPUT2, (short)20);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "input1 = 20");
+    WKPF.setPropertyShort(wuclassInstanceMath, WKPF.PROPERTY_MATH_INPUT3, (short)10);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "input1 = 10");
+    WKPF.setPropertyShort(wuclassInstanceMath, WKPF.PROPERTY_MATH_INPUT4, (short)30);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "input1 = 30");
+    wuclassInstanceMath.update();
+    assertEqual(WKPF.getPropertyShort(wuclassInstanceMath, WKPF.PROPERTY_MATH_OUTPUT), (short)40, "output should be 40.");
+    WKPF.setPropertyShort(wuclassInstanceMath, WKPF.PROPERTY_MATH_OPERATOR, WKPF.ENUM_MATH_OPERATOR_MIN);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "operator = MIN");
+    wuclassInstanceMath.update();
+    assertEqual(WKPF.getPropertyShort(wuclassInstanceMath, WKPF.PROPERTY_MATH_OUTPUT), (short)10, "output should be 10.");
+    WKPF.setPropertyShort(wuclassInstanceMath, WKPF.PROPERTY_MATH_OPERATOR, WKPF.ENUM_MATH_OPERATOR_AVG);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "operator = AVG");
+    wuclassInstanceMath.update();
+    assertEqual(WKPF.getPropertyShort(wuclassInstanceMath, WKPF.PROPERTY_MATH_OUTPUT), (short)25, "output should be 25.");
+    WKPF.setPropertyShort(wuclassInstanceMath, WKPF.PROPERTY_MATH_OPERATOR, WKPF.ENUM_MATH_OPERATOR_ADD);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "operator = ADD");
+    wuclassInstanceMath.update();
+    assertEqual(WKPF.getPropertyShort(wuclassInstanceMath, WKPF.PROPERTY_MATH_OUTPUT), (short)100, "output should be 100.");
+    WKPF.setPropertyShort(wuclassInstanceMath, WKPF.PROPERTY_MATH_OPERATOR, WKPF.ENUM_MATH_OPERATOR_SUB);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "operator = SUB");
+    wuclassInstanceMath.update();
+    assertEqual(WKPF.getPropertyShort(wuclassInstanceMath, WKPF.PROPERTY_MATH_OUTPUT), (short)20, "output should be 20.");
+    WKPF.setPropertyShort(wuclassInstanceMath, WKPF.PROPERTY_MATH_OPERATOR, WKPF.ENUM_MATH_OPERATOR_DIVIDE);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "operator = DIVIDE");
+    wuclassInstanceMath.update();
+    assertEqual(WKPF.getPropertyShort(wuclassInstanceMath, WKPF.PROPERTY_MATH_OUTPUT), (short)2, "output should be 2.");
+    WKPF.setPropertyShort(wuclassInstanceMath, WKPF.PROPERTY_MATH_INPUT3, (short)1);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "input1 = 1");
+    WKPF.setPropertyShort(wuclassInstanceMath, WKPF.PROPERTY_MATH_INPUT4, (short)3);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "input1 = 3");
+    WKPF.setPropertyShort(wuclassInstanceMath, WKPF.PROPERTY_MATH_OPERATOR, WKPF.ENUM_MATH_OPERATOR_MULTIPLY);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "operator = MULTIPLY");
+    wuclassInstanceMath.update();
+    assertEqual(WKPF.getPropertyShort(wuclassInstanceMath, WKPF.PROPERTY_MATH_OUTPUT), (short)2400, "output should be 2400.");
+*/
+
+    //LOGICAL TEST
+    /*VirtualWuObject wuclassInstanceLogical = new VirtualLogicalWuObject();
+    WKPF.registerWuClass(WKPF.WUCLASS_LOGICAL, GENERATEDVirtualLogicalWuObject.properties);
     assertEqual(WKPF.getErrorCode(), WKPF.OK, "Registering VirtualLogicalWuObject.");
     WKPF.createWuObject((short)WKPF.WUCLASS_LOGICAL, (byte)0x21, wuclassInstanceLogical);
     assertEqual(WKPF.getErrorCode(), WKPF.OK, "Creating wuobject for virtual logical wuclass at port 0x21.");
 
     WKPF.setPropertyShort(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_OPERATOR, WKPF.ENUM_LOGICAL_OPERATOR_AND);
     assertEqual(WKPF.getErrorCode(), WKPF.OK, "operator = AND");
-    WKPF.setPropertyBoolean(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_INPUT1, 1);
-    assertEqual(WKPF.getErrorCode(), WKPF.OK, "INPUT1 = 1");
-    WKPF.setPropertyBoolean(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_INPUT2, 0);
-    assertEqual(WKPF.getErrorCode(), WKPF.OK, "INPUT2 = 0");
-    WKPF.setPropertyBoolean(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_INPUT3, 0);
-    assertEqual(WKPF.getErrorCode(), WKPF.OK, "INPUT3 = 0");
-    WKPF.setPropertyBoolean(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_INPUT4, 1);
-    assertEqual(WKPF.getErrorCode(), WKPF.OK, "INPUT4 = 1");
+    WKPF.setPropertyBoolean(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_INPUT1, false);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "INPUT1 = false");
+    WKPF.setPropertyBoolean(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_INPUT2, false);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "INPUT2 = false");
+    WKPF.setPropertyBoolean(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_INPUT3, false);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "INPUT3 = false");
+    WKPF.setPropertyBoolean(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_INPUT4, true);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "INPUT4 = true");
     wuclassInstanceLogical.update();
     assertEqualBoolean(WKPF.getPropertyBoolean(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_OUTPUT), false, "output should be false.");
-*/
+    WKPF.setPropertyShort(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_OPERATOR, WKPF.ENUM_LOGICAL_OPERATOR_OR);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "operator = OR");
+    wuclassInstanceLogical.update();
+    assertEqualBoolean(WKPF.getPropertyBoolean(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_OUTPUT), true, "output should be true.");
+    WKPF.setPropertyShort(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_OPERATOR, WKPF.ENUM_LOGICAL_OPERATOR_NOT);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "operator = NOT");
+    wuclassInstanceLogical.update();
+    assertEqualBoolean(WKPF.getPropertyBoolean(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_OUTPUT), true, "output should be true.");
+    WKPF.setPropertyShort(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_OPERATOR, WKPF.ENUM_LOGICAL_OPERATOR_XOR);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "operator = XOR");
+    wuclassInstanceLogical.update();
+    assertEqualBoolean(WKPF.getPropertyBoolean(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_OUTPUT), true, "output should be true.");
+    WKPF.setPropertyShort(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_OPERATOR, WKPF.ENUM_LOGICAL_OPERATOR_AND);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "operator = AND");
+    WKPF.setPropertyBoolean(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_INPUT1, true);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "INPUT1 = true");
+    WKPF.setPropertyBoolean(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_INPUT2, true);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "INPUT2 = true");
+    WKPF.setPropertyBoolean(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_INPUT3, true);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "INPUT3 = true");
+    WKPF.setPropertyBoolean(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_INPUT4, true);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "INPUT4 = true");
+    wuclassInstanceLogical.update();
+    assertEqualBoolean(WKPF.getPropertyBoolean(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_OUTPUT), true, "output should be true.");
+    WKPF.setPropertyShort(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_OPERATOR, WKPF.ENUM_LOGICAL_OPERATOR_OR);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "operator = OR");
+    wuclassInstanceLogical.update();
+    assertEqualBoolean(WKPF.getPropertyBoolean(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_OUTPUT), true, "output should be true.");
+    WKPF.setPropertyShort(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_OPERATOR, WKPF.ENUM_LOGICAL_OPERATOR_NOT);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "operator = NOT");
+    wuclassInstanceLogical.update();
+    assertEqualBoolean(WKPF.getPropertyBoolean(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_OUTPUT), false, "output should be false.");
+    WKPF.setPropertyShort(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_OPERATOR, WKPF.ENUM_LOGICAL_OPERATOR_XOR);
+    assertEqual(WKPF.getErrorCode(), WKPF.OK, "operator = XOR");
+    wuclassInstanceLogical.update();
+    assertEqualBoolean(WKPF.getPropertyBoolean(wuclassInstanceLogical, WKPF.PROPERTY_LOGICAL_OUTPUT), false, "output should be false.");*/
+
     System.out.println("WuKong WuClass Framework test - done. Passed:" + passedCount + " Failed:" + failedCount);
   }
 }
