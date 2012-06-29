756e0978af47301ed2d93edd880a0cd12fbaa180
Penn Su <pennsuaccount@gmail.com>
2012-06-29 21:56:45 +0800

    added the first prototype of the master server

---
+++ master/master_server.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = 'bog'
+ALLOWED_EXTENSIONS = set(['bog'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+  return '.' in filename and \
+      filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route("/", methods=['POST'])
+def upload_bog():
+  file = request.files['file']
+  if file and allowed_file(file.filename):
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return {'status': 0}
+
+if __name__ == "__main__":
+  app.run()
