502ac2ae70b21766e53255647f8e520f4d5cc520
Penn Su <pennsuaccount@gmail.com>
2012-07-02 17:38:12 +0800

    added to unzip and parse file.xml from the bog file

--- master/master_server.py
+++ master/master_server.py
@@ -1,7 +1,9 @@
-import os
-import sys
-from flask import Flask, request, redirect, url_for
+# author: Penn Su
+# reference url: http://flask.pocoo.org/docs/patterns/fileuploads/
+import os, sys, zipfile
+from flask import Flask, request, redirect, url_for, render_template
 from werkzeug import secure_filename
+from xml.dom.minidom import parse
 
 UPLOAD_FOLDER = 'bog'
 ALLOWED_EXTENSIONS = set(['bog'])
@@ -16,15 +18,23 @@ def allowed_file(filename):
   return '.' in filename and \
       filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+@app.route("/", methods=['GET'])
+def display_master():
+  return render_template('upload.html')
+
 @app.route("/", methods=['POST'])
 def upload_bog():
   file = request.files['file']
   if file and allowed_file(file.filename):
     filename = secure_filename(file.filename)
     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    return 'yes'
+    z = zipfile.Zipfile(file)
+    print z
+    dom = parse(z.extract('file.xml'))
+    print dom
+    return jsonify(status=0)
   else:
-    return 'no'
+    return jsonify(status=1)
 
 if __name__ == "__main__":
   app.run(host=IP)
