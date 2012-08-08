f7e605039de406ece1a911ad3b494044e28752e1
Penn Su <pennsuaccount@gmail.com>
2012-08-08 12:22:09 +0800

    incorporate fbp to application list

--- master/static/js/application.js
+++ master/static/js/application.js
@@ -5,6 +5,19 @@ $(document).ready(function() {
 
 function application_init()
 {
+	obj = $('#menu');
+	obj.empty();
+	obj.append('<button id=appadd class=appadd></button>')
+  obj.click(function() {
+    $.post('/application/new', function(data) {
+      if (data.status == '1') {
+        alert(data.mesg);
+      }
+      else {
+        application_fill();
+      }
+    });
+  });
 	application_fill();
 	/*
 	application_fillList([
@@ -29,6 +42,8 @@ function application_fill()
 {
 	$.ajax({
 		url: '/application/json',
+    type: 'POST',
+    dataType: 'json',
 		success: function(r) {
 			application_fillList(r);
 		}
@@ -43,9 +58,6 @@ function application_fillList(r)
 	var m = $('#content');
 	var obj;
 
-	obj = $('#menu');
-	obj.empty();
-	obj.append('<button id=appadd class=appadd></button>')
 	m.empty();
 	m.html('<table id=applist></table>');
 	applist = $('#applist');
@@ -59,9 +71,25 @@ function application_fillList(r)
 		$('#appact'+i).append('<button class=appdeploy id=appdeploy'+i+'></button>');
 		$('#appact'+i).append('<button class=appdel id=appdel'+i+'></button>');
 		obj = $('#appname'+i);
-		obj.html(r[i].name);
+		obj.html('<a href="#">' + r[i].name + '</a>');
+    var index = i;
+    obj.click(function() {
+      application_setupLink(r, index);
+    });
 		application_setupButtons(i);
 	}
+}
+
+function application_setupLink(r, i)
+{
+  $.post('/application/' + r[i].id, function() {
+    // create application
+    $('#content').html('<div id="topbar"></div><iframe width="100%" height="100%" src="/application/' + r[i].id + '/fbp/load"></iframe>');
+    $('#topbar').append($('<button id="back">Back</button>'));
+    $('#topbar #back').click(function() {
+      application_fill();
+    });
+  });
 }
 
 function application_setupButtons(i)
