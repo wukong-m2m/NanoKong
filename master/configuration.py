08e4a092dd1ad233b96c6f914901bbcf91444401
Sen Zhou <senatorjo@dhcp-v003-253.mobile.uci.edu>
2012-09-30 01:41:35 -0700

    Settings of DEVICE, DEFAULT_CLASS, DISCOVER_FLAGS, PORT moved from vm/apps/base/avr_mega2560/Makefile, vm/build/avr_mega2560/Makefile to config/master.cfg. Using ConfigObj[easy_install ConfigObj] for config file without sections in order to make master.cfg Makefile compatible.

--- master/configuration.py
+++ master/configuration.py
@@ -1,10 +1,9 @@
-import ConfigParser
+from configobj import ConfigObj
 ZWAVE_GATEWAY_IP = None
 
 def readConfig():
-	config = ConfigParser.RawConfigParser()
-	config.read('../config/master.cfg')
+	config = ConfigObj('../config/master.cfg')
 	#get zwave_ip from network section
 	global ZWAVE_GATEWAY_IP 
-	ZWAVE_GATEWAY_IP = config.get("network", 'ZWAVE_GATEWAY_IP')
+	ZWAVE_GATEWAY_IP = config.get('ZWAVE_GATEWAY_IP')
 	print "zwave gateway IP: "+ZWAVE_GATEWAY_IP
