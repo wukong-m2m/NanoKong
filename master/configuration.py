79275467e5e90dbb0666108e25678c16cf49d7e2
Penn Su <pennsuaccount@gmail.com>
2012-10-03 23:48:16 +0800

    just making sure that configuration file is loaded

--- master/configuration.py
+++ master/configuration.py
@@ -1,8 +1,13 @@
+import os
 from configobj import ConfigObj
 ZWAVE_GATEWAY_IP = None
 
+CONFIG_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'config', 'master.cfg')
+print CONFIG_PATH
+
 def readConfig():
-    config = ConfigObj('../config/master.cfg')
+    config = ConfigObj(CONFIG_PATH)
+    print config
     #get zwave_ip from network section
     global ZWAVE_GATEWAY_IP 
     ZWAVE_GATEWAY_IP = config.get('ZWAVE_GATEWAY_IP')
