9bb5ee0c694cbb6d5208c4303a1c72afc6463e2d
Penn Su <pennsuaccount@gmail.com>
2012-10-11 11:16:28 +0800

    add the missing all_wutypes and all_wuclasses

--- tools/python/wkpf.py
+++ tools/python/wkpf.py
@@ -414,3 +414,7 @@ def parseXMLString(xml_string, **kwargs):
 
 def parseXML(xml_path, **kwargs):
     return parseXMLString(open(xml_path).read(), **kwargs)
+
+rootpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..")
+all_wuclasses = parseXML(os.path.join(rootpath, "ComponentDefinitions", "WuKongStandardLibrary.xml")).values()
+all_wutypes = parseXML(os.path.join(rootpath, "ComponentDefinitions", "WuKongStandardLibrary.xml"), type='wutype').values()
