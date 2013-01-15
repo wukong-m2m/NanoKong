c75d5634c7d0df9cec16ac37e8adcec73802b493
Penn Su <pennsuaccount@gmail.com>
2013-01-15 14:32:28 +0800

    added methods to blink LEDs

---
+++ vm/src/led.c
@@ -0,0 +1,38 @@
+#include "avr/io.h"
+#include "led.h"
+
+#define INTERVAL 250
+
+void delay_ms(uint16_t ms) {
+    uint16_t delay_count = F_CPU / 17500;
+    volatile uint16_t i;
+
+    while (ms != 0) {
+        for (i=0; i!=delay_count; i++);
+        ms--;
+    }
+}
+
+void blink_once(LED which)
+{
+    blink(which, 1, INTERVAL);
+}
+
+void blink_twice(LED which)
+{
+    blink(which, 2, INTERVAL);
+}
+
+void blink_thrice(LED which)
+{
+    blink(which, 3, INTERVAL);
+}
+
+void blink(LED which, uint8_t times, uint8_t interval)
+{
+    for (int i=0; i<times; ++i) {
+        output_low(PORTK, which);
+        delay_ms(interval);
+        output_high(PORTK, which);
+    }
+}
