f239d2f008199c9b9f66bd85973a5a3ab29b04a5
Penn Su <pennsuaccount@gmail.com>
2013-03-25 04:01:15 +0800

    added a new unit test for generateJava and throw in a real application used in deployment

--- master/tests/unit_tests/test_wuapplication.py
+++ master/tests/unit_tests/test_wuapplication.py
@@ -21,39 +21,45 @@ xml = '''
         <location requirement="WuKong" />
         <group_size requirement="1" />
         <reaction_time requirement="2" />
-        <actionProperty output="gg"  />
-        <signalProperty output="gg"  />
+        <actionProperty output=""  />
+        <signalProperty output=""  />
     </component>
     <component type="Light_Sensor" instanceId="21" x="76" y="397" w="100" h="100">
         <link fromProperty="current_value" toInstanceId="19" toProperty="value"/>
         <location requirement="WuKong" />
         <group_size requirement="2" />
         <reaction_time requirement="2" />
-        <actionProperty refresh_rate="56"  />
-        <signalProperty current_value="11" refresh_rate="323"  />
+        <actionProperty refresh_rate=""  />
+        <signalProperty current_value="" refresh_rate=""  />
     </component>
     <component type="Light_Actuator" instanceId="22" x="633" y="224" w="100" h="100">
         <location requirement="WuKong" />
         <group_size requirement="2" />
         <reaction_time requirement="2" />
-        <actionProperty on_off="true"  />
-        <signalProperty on_off="false"  />
+        <actionProperty on_off=""  />
+        <signalProperty on_off=""  />
     </component>
 </page>
 </application>
 '''
 
 class TestWuApplication(unittest.TestCase):
-  def test_parseApplication(self):
-    application = WuApplication('455', '123')
-    application.setFlowDom(parseString(xml))
-    application.parseApplication()
+  def setUp(self):
+    self.application = WuApplication('455', '123')
+    self.application.setFlowDom(parseString(xml))
+    self.application.parseApplication()
 
-    self.assertEqual(application.name, '123')
-    self.assertEqual(len(application.changesets.components), 4)
-    self.assertTrue((u'output', u'gg') in application.changesets.components[1].properties_with_default_values)
-    self.assertTrue((u'refresh_rate', u'323') in application.changesets.components[2].properties_with_default_values)
-    self.assertTrue((u'on_off', u'false') in application.changesets.components[3].properties_with_default_values)
+  def test_parseApplication(self):
+    self.assertEqual(self.application.name, '123')
+    self.assertEqual(len(self.application.changesets.components), 4)
+    self.assertTrue((u'output', u'gg') in self.application.changesets.components[1].properties_with_default_values)
+    self.assertTrue((u'refresh_rate', u'323') in self.application.changesets.components[2].properties_with_default_values)
+    self.assertTrue((u'on_off', u'false') in self.application.changesets.components[3].properties_with_default_values)
+
+  def test_generateJava(self):
+    self.application.generateJava()
+
+    self.assertEqual(self.application.name, '123')
 
 if __name__ == '__main__':
   unittest.main()
